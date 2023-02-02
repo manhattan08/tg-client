55e18492fb64c14499f0d75bccabae9a437f8699
Andrey Zaets <kn211_zayu@student.ztu.edu.ua>
2023-02-02 13:52:02 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import {useEffect} from "react";
-
+import {useTelegram} from "./hooks/useTelegram";
 
 function App() {
+    const {tg,onToggleButton} = useTelegram()
     useEffect(()=>{
         tg.ready();
     },[])
   return (
     <div className="App">
-      <button onClick={onClose}>Close</button>
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
